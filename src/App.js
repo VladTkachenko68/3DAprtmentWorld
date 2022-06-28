e67291bbc822ad67696f1c9e7ee18af00dfcc583
Full Stack Developer <99469914+talented0312@users.noreply.github.com>
2022-06-28 06:35:18 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ function LittlestTokyo({ hover }) {
   // })
   // return <primitive object={scene} {...props} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)}/>;
   return (
-    <Suspense fallback={null}>
+    <Suspense fallback={<h1>Loading profile...</h1>}>
     <group ref={ref}>
     <primitive object={scene} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)}/>
       {/* <ReactAtom position={[35, -20, 0]} scale={[1, 0.5, 1]} /> */}
